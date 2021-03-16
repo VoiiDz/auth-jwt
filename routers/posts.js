3117e1bd27fdbfe3aae48789c542dc79f4e5159d
VoiiDz <moncifjb@gmail.com>
2021-03-16 22:23:11 +0100

    finish up the jwt

--- routers/posts.js
+++ routers/posts.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
+const auth = require('./privateRoute');
 
 
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
     res.json({
         posts: {
-            title: 'first Post', desc: 'private data'
+            title: 'first Post', 
+            desc: 'private data',
+            user: req.user
         }
     });
 });
