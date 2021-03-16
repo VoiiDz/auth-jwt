84dce8aec674df1453ba9dca3ea5652bbf3d53f1
VoiiDz <moncifjb@gmail.com>
2021-03-16 21:47:22 +0100

    jwt and token initialization

--- routers/auth.js
+++ routers/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { registerVal, loginVal } = require('../validation');
 
 router.post('/register', async (req, res) => {
@@ -44,7 +45,9 @@ router.post('/login', async (req, res) => {
     const validPw= await bcrypt.compare(req.body.password, user.password);
     if (!validPw) return res.status(400).send('Invalid password');
 
-    res.send('Logged in');
+    //CREATING AND ASSIGNING A TOKEN
+    const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
 });
 
 module.exports = router;
